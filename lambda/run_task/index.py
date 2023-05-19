a6fe0043a5094ad44fbd8064e3564f63589fdea4
Violet Vivirand <purpleslovemail@gmail.com>
2023-05-19 14:11:29 +0800

    Use jsonpickle to serialize, remove requests

--- lambda/run_task/index.py
+++ lambda/run_task/index.py
@@ -1,18 +1,21 @@
+# Blank Lambda function ref: https://github.com/awsdocs/aws-lambda-developer-guide/blob/main/sample-apps/blank-python/function/lambda_function.py
+# SQS sample code ref: https://docs.aws.amazon.com/lambda/latest/dg/with-sqs-create-package.html#with-sqs-example-deployment-pkg-python
+import logging
 import os
-import boto3
-import requests
-import json
+import boto3  # type: ignore
+import jsonpickle  # type: ignore
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 sqs = boto3.client('sqs')
-# [TODO] Isolate the tasks' content from here to another document
-msgs = ["Hello from Lambda #1!", "Hello from Lambda #2!"]
 
-# [TODO] Add Logging
 def handler(event, context):    
-    return {
-        "statusCode": 200,
-        "headers": {
-            "Content-Type": "application/json"
-        },
-        "body": json.dumps("Message(s) received successfully", indent=4, sort_keys=True),
-    }
+    logger.info('## ENVIRONMENT VARIABLES\r' + jsonpickle.encode(dict(**os.environ)))
+    logger.info('## EVENT\r' + jsonpickle.encode(event))
+    logger.info('## CONTEXT\r' + jsonpickle.encode(context))
+    
+    for record in event['Records']:
+        print("Task received:")
+        payload = record["body"]
+        print(str(payload))
--- lambda/send_task/index.py
+++ lambda/send_task/index.py
@@ -1,26 +1,32 @@
+# Blank Lambda function ref: https://github.com/awsdocs/aws-lambda-developer-guide/blob/main/sample-apps/blank-python/function/lambda_function.py
+import logging
 import os
-import boto3
-import requests
-import json
+import boto3  # type: ignore
+import jsonpickle  # type: ignore
 
-QUEUE_URL = os.environ["QUEUE_URL"]
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+queue_url = os.environ["QUEUE_URL"]
 sqs = boto3.client('sqs')
-# [TODO] Isolate the tasks' content from here to another document
-msgs = ["Hello from Lambda #1!", "Hello from Lambda #2!"]
+tasks = ["Hello from Lambda #1!", "Hello from Lambda #2!"]
+logger.info('## TASKS\r' + jsonpickle.encode(tasks))
 
-# [TODO] Add Logging
 def handler(event, context):
-    for msg in msgs:
-        response = sqs.send_message(
-            QueueUrl=QUEUE_URL,
-            MessageBody=(msg),
-            MessageGroupId='1337',
-        )
+    logger.info('## ENVIRONMENT VARIABLES\r' + jsonpickle.encode(dict(**os.environ)))
+    logger.info('## EVENT\r' + jsonpickle.encode(event))
+    logger.info('## CONTEXT\r' + jsonpickle.encode(context))
+
+    # Send tasks to queue
+    for task in tasks:
+        try:
+            response = sqs.send_message(
+                QueueUrl=queue_url,
+                MessageBody=(task),
+                MessageGroupId='1337',
+            )
+            logger.info('## SQS RESPONSE\r' + jsonpickle.encode(response))
+        except Exception as e:
+            logger.error(e)
+            raise e
     
-    return {
-        "statusCode": 200,
-        "headers": {
-            "Content-Type": "application/json"
-        },
-        "body": json.dumps("Message(s) sent successfully", indent=4, sort_keys=True),
-    }
